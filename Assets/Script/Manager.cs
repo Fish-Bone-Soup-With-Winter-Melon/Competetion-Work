905b5f23fb1bc66535de04928e35e19893a3d782
rosist <2779477248@qq.co,[D[D[D[D[D[D[D[C[C[C[C[C[C[m>
2024-01-23 20:44:18 +0800

    Fix_ map to json

---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,47 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Manager : MonoBehaviour
+{
+    public Tilemap Ground, Ice;
+    public List<TileBase> tiles = new List<TileBase>();
+    [ContextMenu("save")]
+
+    public void test()
+    {
+        TileMapHandler Tilemaphandler = new TileMapHandler(Ground , Ice);
+        Tilemaphandler.MapToJson("map_0", new Vector2Int(20 , 11));
+
+    }
+    [ContextMenu("Load")]
+    public void Load()
+    {
+        TileMapHandler Tilemaphandler = new TileMapHandler();
+        Map map = Tilemaphandler.getMapByName("map_0");
+        if(map == null)
+        {
+            Debug.Log("¶ÁÈ¡ÎÄ¼þÊ§°Ü");
+            return;
+        }
+        foreach (Tile item in map.groundTiles)
+        {
+            Ground.SetTile(new Vector3Int(item.x, item.y, 0) ,tiles.Where(i => i.name == item.tileName).FirstOrDefault());
+        }
+        foreach (Tile item in map.iceTiles)
+        {
+            Ice.SetTile(new Vector3Int(item.x, item.y, 0), tiles.Where(i => i.name == item.tileName).FirstOrDefault());
+        }
+    }
+
+    [ContextMenu("clear")]
+
+    public void test2()
+    {
+        Ground.ClearAllTiles();
+        Ice.ClearAllTiles();
+    }
+}
---
+++ Assets/Script/TileMapHandler.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+using LitJson;
+using UnityEngine.TextCore.Text;
+
+public class TileMapHandler
+{
+    public Tilemap Ground, Ice; 
+    public TileMapHandler(Tilemap Ground, Tilemap Ice)
+    {
+        this.Ground = Ground;
+        this.Ice = Ice;
+    }
+    public TileMapHandler()
+    {
+
+    }
+
+    public string MapToJson(string mapName , Vector2Int size)
+    {
+        Map map = new Map();
+        map.mapName = mapName;
+        for(int i = -10; i < size.x - 10; i++)
+        {
+            for(int j = -5; j < size.y - 5; j++)
+            {
+                TileBase tb = Ground.GetTile(new Vector3Int(i, j, 0));
+                if(tb != null){
+                    Tile t = new Tile();
+                    t.tileName = tb.name;
+                    t.x = i;
+                    t.y = j;
+                    map.groundTiles.Add(t);
+                }
+            }
+        }
+        for (int i = -10; i < size.x - 10; i++)
+        {
+            for (int j = -5; j < size.y - 5; j++)
+            {
+                TileBase tb = Ice.GetTile(new Vector3Int(i, j, 0));
+                if (tb != null)
+                {
+                    Tile t = new Tile();
+                    t.tileName = tb.name;
+                    t.x = i;
+                    t.y = j;
+                    map.iceTiles.Add(t);
+                }
+            }
+        }
+
+        UnityEngine.TextAsset textAsset = Resources.Load<UnityEngine.TextAsset>("Maps");
+        AllMaps allMaps = JsonMapper.ToObject<AllMaps>(textAsset.text);
+        Resources.UnloadAsset(textAsset);
+        if(allMaps == null)
+        {
+            allMaps = new AllMaps();
+        }
+        else
+        {
+            var m = allMaps.Maps.Where(item => item.mapName == mapName);
+            if(m.Count() > 0)
+            {
+                allMaps.Maps.Remove(m.FirstOrDefault());
+            }
+        }
+        allMaps.Maps.Add(map);
+        string json = JsonMapper.ToJson(allMaps);
+        File.WriteAllText(Application.dataPath+"/Resources/Maps.json", json);
+        return json;
+    }
+    public Map getMapByName(string mapName) {
+
+        string json = File.ReadAllText(Application.dataPath + "/Resources/Maps.json");
+        AllMaps allMaps = JsonMapper.ToObject<AllMaps>(json);
+        if(allMaps == null)
+        {
+            return null;
+        }
+        else
+        {
+            var m = allMaps.Maps.Where(item => item.mapName == mapName);
+            return m.FirstOrDefault();
+        }
+    }
+}
+
+public class AllMaps {
+    public List<Map> Maps = new List<Map>();
+}
+
+public class Map  {
+    public string mapName;
+    public List<Tile> groundTiles = new List<Tile>();
+    public List<Tile> iceTiles = new List<Tile>();
+}
+
+public class Tile {
+    public string tileName;
+    public int x, y;
+}
+
+
